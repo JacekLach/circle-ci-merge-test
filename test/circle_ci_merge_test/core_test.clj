cc53e76ab024b3e6295dbea809cd773eae28f2d0
Jacek Lach <jacek@lshift.net>
2013-01-22 10:35:13 +0000

    Master test

--- test/circle_ci_merge_test/core_test.clj
+++ test/circle_ci_merge_test/core_test.clj
@@ -2,6 +2,9 @@
   (:use clojure.test
         circle-ci-merge-test.core))
 
-(deftest a-test
+(defn foo []
+  2)
+
+(deftest master-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (is (= 0 0))))
